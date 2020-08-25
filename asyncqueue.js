55ddf5ec9ddbf0ef96d12c7837a0b7cf618ae0b1
Kang Chen <rainyjune@live.cn>
2020-08-25 14:31:17 +0800

    Update asyncqueue.js

--- asyncqueue.js
+++ asyncqueue.js
@@ -1,1 +1,22 @@
-alert('asyncqueue.js');
+var queue = function() {
+  var taskList = [];
+  var isRunning = false;
+  
+  return {
+    enqueue: function(task) {
+      taskList.push(task);
+    },
+    run: function() {
+      if (taskList.length === 0 || isRunning) return ;
+      var task = taskList.shift();
+      task();
+    },
+    inspect: function() {
+      console.log('taskList:', taskList, ' isRunning:', isRunning);
+    },
+    empty: function() {
+      taskList.length = 0;
+      isRunning = false;
+    }
+  };
+};
